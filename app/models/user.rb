94706afd07094e84a1db96329226fed068babccf
Latteholic <cafelatte@gmail.com>
2020-08-23 00:56:39 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  validates :name, presence: true
+  
   has_many :rooms, through: :users_rooms_table
   has_many :messages
 
